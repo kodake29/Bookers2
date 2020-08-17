9a7ac430908ad7ad083cc7cf0e38a1b1f6810863
vagrant <vagrant@localhost.localdomain>
2020-08-17 10:50:52 +0000

    [finish] 5-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/update'
+	root 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
