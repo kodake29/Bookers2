4ae7170c63837d61e1805b5fe40579c612263112
vagrant <vagrant@localhost.localdomain>
2020-08-20 12:28:49 +0000

    [finish]8/20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+    if sign_in
+    user_path(current_user), notice: 'Signed in successfully.'
+  end
+end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
+
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Signed out successfully.'
+  end
+
+  def new
+  end
+
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
   	@users = User.all
     @book_new =Book.new
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@books = @user.books.page(params[:page]).reverse_order
-  	@book = Book.new
+  	@book_new = Book.new
   end
 
    def edit
